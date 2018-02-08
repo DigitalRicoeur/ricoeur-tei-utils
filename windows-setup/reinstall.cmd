741d05d163534ec9a91a1af72ed00ce1d542d2ae
Philip McGrath <philip@philipmcgrath.com>
2018-02-07 21:09:42 -0600

    Try double quotes

--- windows-setup/reinstall.cmd
+++ windows-setup/reinstall.cmd
@@ -10,8 +10,8 @@ ECHO This batch file does not handle pulling updates to the git repository.
 ECHO Press enter to begin.
 PAUSE
 cd ..
-'C:\Program Files\Racket\raco.exe' pkg remove --force tei-utils
-'C:\Program Files\Racket\raco.exe' pkg install -i
+"C:\Program Files\Racket\raco.exe" pkg remove --force tei-utils
+"C:\Program Files\Racket\raco.exe" pkg install -i
 ECHO Press enter to close this window.
 PAUSE
 
