d810c436567ba15dd16378fc036535d08f638091
Philip McGrath <philip@philipmcgrath.com>
2018-05-29 12:15:49 -0500

    Increment Adjutor version in preparation for Racket 7

--- info.rkt
+++ info.rkt
@@ -10,7 +10,7 @@
 
 ;; Dependencies:
 (define deps '(("base" #:version "6.12")
-               ("adjutor" #:version "0.2.3")
+               ("adjutor" #:version "0.2.4")
                "functional-lib"
                "roman-numeral"
                "gregor-lib"
