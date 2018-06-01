f1a66a1a378e5029b741c391529e7f4dc492b564
Philip McGrath <philip@philipmcgrath.com>
2018-06-01 14:34:06 -0500

    Another test for Windows GUI bug

--- windows-splash-debug.rkt
+++ windows-splash-debug.rkt
@@ -1,41 +1,22 @@
 #lang racket/gui
 
 (require ricoeur/tei/tools/tei-lint/lib
+         ricoeur/tei/tools/tei-lint/splash
          )
 
-(define splash%
-  (class frame%
-    (super-new)
-    (init subtitle bitmap message)
+(define splash-frame%
+  (class abstract-splash-frame%
+    (super-new [label "TEI Lint"]
+               [subtitle "TEI Lint"]
+               [bitmap photo-bitmap]
+               [height (floor (* 5/4 (send photo-bitmap get-height)))])
     (inherit show)
-    (let ([row (new horizontal-pane% [parent this])])
-      (new message%
-           [parent row]
-           [label bitmap])
-      (define col
-        (new vertical-pane% [parent row]))
-      (new message%
-           [parent col]
-           [label "Digital Ricœur"])
-      (new message%
-           [parent col]
-           [label subtitle])
-      (new editor-message%
-           [parent col]
-           [content message])
-      (on-initialize-col col))
-    (define/public (on-initialize-col col)
-      (void))
-    (show #t)
-    #|END splash%|#))
+    (define/override-final (on-choose-directory dir)
+      (show #f)
+      #;(new directory-frame% [dir dir]))))
 
 
 
 
-(define f
-  (new splash%
-       [label "Label"]
-       [subtitle "Subtitle"]
-       [message "Message"]
-       [bitmap "Bitmap"]))
+(void (new splash-frame%))
 
