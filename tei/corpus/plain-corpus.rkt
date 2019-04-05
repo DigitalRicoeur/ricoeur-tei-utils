9cebd7de83af0bcc0ab7dd75e55048beb68e793a
Philip McGrath <philip@philipmcgrath.com>
2019-04-04 23:49:00 -0400

    WIP on a better corpus extension mechanism

--- tei/corpus/plain-corpus.rkt
+++ tei/corpus/plain-corpus.rkt
@@ -5,9 +5,15 @@
          racket/set
          racket/promise
          ricoeur/tei/base
-         )
+         syntax/parse/define
+         racket/stxparam
+         racket/splicing
+         (for-syntax racket/base
+                     syntax/transformer))
 
 (provide checksum-table/c
+         corpus-mixin
+         super-docs
          (contract-out
           [plain-corpus%
            (class/c (init [docs (instance-set/c tei-document?)]))]
@@ -17,38 +23,21 @@
           [corpus-get-checksum-table
            (-> (is-a?/c plain-corpus%)
                checksum-table/c)]
-          [make-corpus-mixin
-           (let ([corpus-mixin/c (make-mixin-contract plain-corpus%)]
-                 [initialize-this/c (->m (instance-set/c tei-document?)
-                                         any)])
-             (->i {[k member-name-key?]}
-                  [_ (k)
-                     (let ()
-                       (define-member-name initialize-this k)
-                       (and/c corpus-mixin/c
-                              (-> (class/c
-                                   (absent initialize-this))
-                                  (class/c
-                                   (override
-                                     [initialize-this
-                                      initialize-this/c])))))]))]
           ))
 
 (define/final-prop checksum-table/c
   (hash/c symbol? symbol?
           #:immutable #t))
 
-(define-member-name on-initialize (generate-member-key))
+(define-local-member-name initialize)
 
 (define plain-corpus%
   (class* object% {(interface ()
                      [get-instance-info-set
                       (->m (instance-set/c))]
                      [get-checksum-table
-                      (->m (hash/c symbol?
-                                   symbol?
-                                   #:immutable #t))]
-                     on-initialize)}
+                      (->m checksum-table/c)]
+                     initialize)}
     (super-new)
     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     ;; Initialization:
@@ -56,27 +45,133 @@
     (define info
       (instance-set
        (set-map docs get-plain-instance-info)))
-    ;;(printf "docs: ~v\n" (length (set->list docs)))
-    ;;(printf "info: ~v\n" (length (set->list info)))
     (define pr:checksum-table
       (delay/thread
        (for/hasheq ([doc (in-instance-set docs)])
          (values (instance-title/symbol doc)
                  (tei-document-checksum doc)))))
     ;; Must come at the end of initialization:
-    (on-initialize docs)
+    (initialize docs)
     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     ;; Methods:
-    (define/pubment (on-initialize docs)
-      (inner (void) on-initialize docs))
     (define/public-final (get-instance-info-set)
       info)
     (define/public-final (get-checksum-table)
       (force pr:checksum-table))
+    (define/pubment (initialize docs)
+      (inner (void) initialize docs))
     #|END class plain-corpus%|#))
 
+(define plain-corpus<%>
+  (class->interface plain-corpus%))
 
 
+(define gen:get-instance-info-set
+  (generic plain-corpus<%> get-instance-info-set))
+(define (corpus-get-instance-info-set c)
+  (send-generic c gen:get-instance-info-set))
+
+(define gen:get-checksum-table
+  (generic plain-corpus<%> get-checksum-table))
+(define (corpus-get-checksum-table c)
+  (send-generic c gen:get-checksum-table))
+
+;                                          
+;                                          
+;                                          
+;                                          
+;              ;               ;           
+;              ;;              ;;          
+;  ; ;; ;;  ;;;;;  ;;   ;;  ;;;;;   ;; ;   
+;  ;; ;; ;     ;;    ;  ;      ;;   ;;; ;  
+;  ;; ;; ;;    ;;    ; ;       ;;   ;;  ;; 
+;  ;; ;; ;;    ;;     ;        ;;   ;;  ;; 
+;  ;; ;; ;;    ;;    ; ;       ;;   ;;  ;; 
+;  ;; ;; ;;    ;;   ;   ;      ;;   ;;  ;; 
+;  ;; ;; ;;    ;;  ;;   ;;     ;;   ;;  ;; 
+;                                          
+;                                          
+;                                          
+;                                          
+
+(define-syntax (super-docs stx)
+  (raise-syntax-error
+   #f "only allowed inside a corpus-mixin expression" stx))
+
+(define-rename-transformer-parameter local-internal-name
+  (make-rename-transformer #'no-local-internal-name))
+
+(define-syntax no-local-internal-name #f)
+
+(define (do-super-docs-ref bx)
+  (or (unbox bx)
+      (raise (exn:fail:contract:variable
+              (string-append
+               "super-docs: undefined;\n"
+               " cannot access documents before super-class initialization")
+              (current-continuation-marks)
+              'super-docs))))
+
+(define-syntax-parser corpus-mixin
+  #:track-literals
+  [(_ (~describe "\"from\" interfaces" [from<%>:expr ...])
+      (~describe "\"to\" interfaces" [to<%>:expr ...])
+      (~describe "#:avoid-bug clause"
+                 [#:avoid-bug (~and src-super-docs (~literal super-docs))])
+      (~describe "class clause" src-class-clause:expr)
+      ...+)
+   ;; this would be much nicer with a fix for any of
+   ;;   - https://github.com/racket/racket/issues/2578
+   ;;   - https://github.com/racket/racket/issues/1898
+   ;;   - https://github.com/racket/racket/issues/2580
+   ;;   - https://github.com/racket/racket/issues/2579
+   #`(let ([src-super-docs 'tmp]) ;; this is so it doesn't get local-expanded
+       (define-local-member-name private-field private-init)
+       (mixin [plain-corpus<%> from<%> ...] [to<%> ...]
+         (field [private-field (box #f)])
+         (init [(src-super-docs private-init)
+                (λ () (do-super-docs-ref private-field))])
+         (define/augment (initialize docs)
+           (set-box! private-field docs)
+           (inner (void) initialize docs))
+         src-class-clause ...
+         (set! private-field #f)))])
+
+#|
+(define-syntax-parser super-docs
+  [(_)
+   #:fail-unless (syntax-local-value #'local-internal-name)
+   "only allowed inside a corpus-mixin expression"
+   #'(do-super-docs-ref local-internal-name)])
+
+(define-rename-transformer-parameter local-internal-name
+  (make-rename-transformer #'no-local-internal-name))
+
+(define-syntax no-local-internal-name #f)
+
+(define-syntax-parser corpus-mixin
+  [(_ (~describe "\"from\" interfaces" [from<%>:expr ...])
+      (~describe "\"to\" interfaces" [to<%>:expr ...])
+      (~describe "class clause" class-clause:expr)
+      ...+)
+   #'(let ()
+       (define-local-member-name private-field private-init)
+         (mixin [plain-corpus<%> from<%> ...] [to<%> ...]
+           ;; or, pass it to the superclass via splicing-parameterize ...
+           (field [private-field (box #f)])
+           (init [private-init
+                  ;; technically, a by-position arg could displace this :(
+                  private-field])
+           (define/augment (initialize docs)
+             (set-box! private-field docs)
+             (inner (void) initialize docs))
+           (splicing-syntax-parameterize ([local-internal-name
+                                           (make-rename-transformer
+                                            #'private-init)])
+             class-clause ...)
+           (set! private-field #f)))])
+|#
+#|
 (define (make-corpus-mixin initialize-this-method-key)
   (define-member-name initialize-this initialize-this-method-key)
   (mixin {(class->interface plain-corpus%)} {}
@@ -86,15 +181,4 @@
     (define/augment (on-initialize docs)
       (initialize-this docs)
       (inner (void) on-initialize docs))))
-
-
-(define gen:get-instance-info-set
-  (generic (class->interface plain-corpus%) get-instance-info-set))
-(define (corpus-get-instance-info-set c)
-  (send-generic c gen:get-instance-info-set))
-
-(define gen:get-checksum-table
-  (generic (class->interface plain-corpus%) get-checksum-table))
-(define (corpus-get-checksum-table c)
-  (send-generic c gen:get-checksum-table))
-
+|#
